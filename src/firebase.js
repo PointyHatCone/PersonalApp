fbd40e6d4ff651cc4d9b6780e04fdbccc0254b30
PointyHatCone <kimbryce2007@gmail.com>
2024-10-27 21:44:48 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBMJHTwehmlyljcdq1QyrgF04OyUDQ3ggk",
-    authDomain: "react-contact-form-a156b.firebaseapp.com",
-    projectId: "react-contact-form-a156b",
-    storageBucket: "react-contact-form-a156b.appspot.com",
-    messagingSenderId: "1060185578491",
-    appId: "1:1060185578491:web:4567d3c3db41b2f32f095e",
-    measurementId: "G-F3WERE1NZ8"
+  authDomain: "react-contact-form-a156b.firebaseapp.com",
+  projectId: "react-contact-form-a156b",
+  storageBucket: "react-contact-form-a156b.appspot.com",
+  messagingSenderId: "1060185578491",
+  appId: "1:1060185578491:web:72ed7eb663c9b3c82f095e",
+  measurementId: "G-11NWMB1BC4"
 };
 
 if (!firebase.apps.length) {
